e9eb97faf893e45fa40ca9309d40d1fda47e2a3e
Jake <work.jacobsmith@gmail.com>
2023-10-01 19:00:24 -0600

    move.py complete & rename.py needs testing

---
+++ move.py
@@ -0,0 +1,41 @@
+import os
+import shutil
+import re
+from datetime import datetime
+
+def extract_date_from_filename(filename):
+    # Attempt to match MM.DD.YYYY, MM-DD-YYYY, etc.
+    date_pattern = re.search(r'(\d{1,2}[.\-]\d{1,2}[.\-]\d{4})', filename)
+    if date_pattern:
+        date_str = date_pattern.group(1).replace('.', '-')
+        try:
+            return datetime.strptime(date_str, "%m-%d-%Y")
+        except ValueError:
+            print(f"Error parsing date from {filename}. Skipping.")
+            return None
+    return None
+
+def move_files_based_on_filename(directory):
+    for filename in os.listdir(directory):
+        filepath = os.path.join(directory, filename)
+
+        # Check if it's a file and has the required extension
+        if os.path.isfile(filepath) and filename.endswith(('.jpeg', '.png', '.pdf')):
+            file_date = extract_date_from_filename(filename)
+            
+            if file_date:
+                # Define the folder name based on the date in the filename
+                folder_name = file_date.strftime("%B-%Y")  # Format: Month-Year
+                folder_path = os.path.join(directory, folder_name)
+
+                # Create the folder if it doesn't exist
+                if not os.path.exists(folder_path):
+                    os.makedirs(folder_path)
+
+                # Move the file to the appropriate folder
+                shutil.move(filepath, os.path.join(folder_path, filename))
+                print(f"Moved {filename} to {folder_name}")
+
+if __name__ == '__main__':
+    # Process all files in the current directory
+    move_files_based_on_filename('.')
---
+++ rename.py
@@ -0,0 +1,45 @@
+import re
+import os
+from PIL import Image
+import pytesseract
+
+def extract_info_from_image(img_path):
+    try:
+        # Open image using PIL
+        with Image.open(img_path) as img:
+            # Use pytesseract to extract text from the image
+            text = pytesseract.image_to_string(img)
+            
+            # Search for a six-digit number
+            six_digit_number = re.search(r'(\b\d{6}\b)', text)
+            six_digit_number = six_digit_number.group(1) if six_digit_number else None
+            
+            # Search for a date in the format MM/DD/YYYY, MM-DD-YYYY, etc.
+            date_pattern = re.search(r'(\d{1,2}[\/\-]\d{1,2}[\/\-]\d{4})', text)
+            date = date_pattern.group(1).replace('/', '.').replace('-', '.').replace(' ', '') if date_pattern else None
+
+            return six_digit_number, date
+    except Exception as e:
+        print(f"Error processing {img_path}: {e}")
+        return None, None
+
+def process_files_in_directory(directory):
+    for filename in os.listdir(directory):
+        # Check if the file has the required extension
+        if filename.endswith((".pdf", ".jpeg", ".png")):
+            filepath = os.path.join(directory, filename)
+            
+            six_digit_number, date = extract_info_from_image(filepath)
+            
+            # If both the six-digit number and date are found, rename the file
+            if six_digit_number and date:
+                new_filename = six_digit_number + '_' + date + os.path.splitext(filename)[1]
+                new_filepath = os.path.join(directory, new_filename)
+                os.rename(filepath, new_filepath)
+                print(f'Renamed {filename} to {new_filename}')
+            else:
+                print(f'Could not find required info in {filename}')
+
+if __name__ == '__main__':
+    # Process all files in the current directory
+    process_files_in_directory('.')
